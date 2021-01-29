050e9b26fbe767aeb04331a633cdf522a9c560df
Sportsguy <mahoward2s@gmail.com>
2021-01-29 08:04:44 -0600

    added constants for mang, int, eng, and started prompt array

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+
+const manager = require('./lib/Manager');
+const engineer = require('./lib/Engineer');
+const intern = require('./lib/Intern');
+
+const promptUser = () =>
+  inquirer.prompt([
+
+  ])
