be783bdc46ee7fed80502c79fdef7bdcdda71a7c
Sportsguy <mahoward2s@gmail.com>
2021-01-29 08:19:13 -0600

    added manager prompts

--- index.js
+++ index.js
@@ -5,7 +5,38 @@ const manager = require('./lib/Manager');
 const engineer = require('./lib/Engineer');
 const intern = require('./lib/Intern');
 
-const promptUser = () =>
-  inquirer.prompt([
 
-  ])
+//Prompt user for manager information
+const managerData = () =>
+    inquirer.prompt([
+        {
+            //Team info
+            type: "input",
+            message: "What is the name of the team?",
+            name: "teamTitle"
+        },
+        {
+            //Manager Information/Role
+            type: "input",
+            message: "Who is the manager of this team?",
+            name: "managerName"
+        },
+        {
+            //Manager ID
+            type: "input",
+            message: "What is the ID of the manager?",
+            name: "managerId"
+        },
+        {
+            //Manager Email
+            type: "input",
+            message: "What is the manager's email?",
+            name: "managerEmail"
+        },
+        {
+            //Manager Office Number
+            type: "input",
+            message: "What is the office number for the manager?",
+            name: "teamTitle"
+        },
+    ])
